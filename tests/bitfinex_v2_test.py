55289df718aef0d01b293bafbdb4c59f503632a0
joachimvalente <joachim.valente@gmail.com>
2017-12-03 03:15:58 -0800

    Rename BitfinexClient to BitfinexV2Client.

--- tests/bitfinex_v2_test.py
+++ tests/bitfinex_v2_test.py
@@ -34,16 +34,16 @@ class SessionMock(requests.Session):
     return ResponseMock(self._status_code, self._response_body)
 
 
-class BitfinexClientTests(unittest.TestCase):
+class BitfinexV2ClientTests(unittest.TestCase):
 
   @mock.patch.object(requests, 'Session', return_value=SessionMock(200, '[1]'))
   def test_platform_status(self, session_mock):
-    client = bitfinex.BitfinexClient()
+    client = bitfinex.BitfinexV2Client()
     self.assertEqual('[1]', client.platform_status())
 
   @mock.patch.object(requests, 'Session', return_value=SessionMock(500, ''))
   def test_platform_status_fails(self, session_mock):
-    client = bitfinex.BitfinexClient()
+    client = bitfinex.BitfinexV2Client()
     self.assertRaisesRegex(requests.exceptions.HTTPError, '500 Server Error',
                            client.platform_status)
 
@@ -52,24 +52,24 @@ class BitfinexClientTests(unittest.TestCase):
       "[['exchange', 'ETH', 10.0, 0, None], "
       "['exchange', 'BTC', 20.0, 0, None]]"))
   def test_wallets(self, session_mock):
-    client = bitfinex.BitfinexClient('key', 'secret')
+    client = bitfinex.BitfinexV2Client('key', 'secret')
     self.assertEqual(
         "[['exchange', 'ETH', 10.0, 0, None], "
         "['exchange', 'BTC', 20.0, 0, None]]", client.wallets())
 
   def test_wallets_unauthenticated_fails(self):
-    client = bitfinex.BitfinexClient()
+    client = bitfinex.BitfinexV2Client()
     self.assertRaises(bitfinex.NotAuthenticatedError, client.wallets)
 
   @mock.patch.object(requests, 'Session', return_value=SessionMock(500, ''))
   def test_wallets_invalid_authentication(self, session_mock):
-    client = bitfinex.BitfinexClient('key', 'wrong-secret')
+    client = bitfinex.BitfinexV2Client('key', 'wrong-secret')
     self.assertRaisesRegex(requests.exceptions.HTTPError, '500 Server Error',
                            client.wallets)
 
   @mock.patch.object(requests, 'Session', return_value=SessionMock(200, '[1]'))
   def test_multiple_requests(self, session_mock):
-    with bitfinex.BitfinexClient('key', 'secret') as client:
+    with bitfinex.BitfinexV2Client('key', 'secret') as client:
       self.assertEqual('[1]', client.wallets())
       self.assertEqual('[1]', client.orders())
 
